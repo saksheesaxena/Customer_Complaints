47f5e7353ed8177349dc8f7891f3ed8b423f707d
saksheesaxena <saksheesaxena33@gmail.com>
2015-08-11 15:02:43 +0530

    Theme and other changes

--- app/src/main/java/com/example/admin/customer_complaints/MainActivity.java
+++ app/src/main/java/com/example/admin/customer_complaints/MainActivity.java
@@ -2,16 +2,26 @@ package com.example.admin.customer_complaints;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity{
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        if(toolbar != null)
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 
     @Override
