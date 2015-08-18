28d8c8ecd096f9b78ce404567b976c5c213f2b5e
saksheesaxena <saksheesaxena33@gmail.com>
2015-08-18 23:47:48 +0530

    Icons

---
+++ app/src/main/java/com/example/admin/customer_complaints/BaseActivity.java
@@ -0,0 +1,82 @@
+package com.example.admin.customer_complaints;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by admin on 18-Aug-15.
+ */
+
+        public abstract class BaseActivity extends AppCompatActivity
+
+            {
+                private Toolbar toolbar;
+                @Override
+           protected void onCreate(Bundle savedInstanceState) {
+                    super.onCreate(savedInstanceState);
+                    setContentView(R.layout.activity_main);
+                    setContentView(getLayoutResource());
+                    toolbar = (Toolbar) findViewById(R.id.tool_bar);
+
+                            if (toolbar != null) {
+                            setSupportActionBar(toolbar);
+                            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+                                      }
+                }
+
+                   protected abstract int getLayoutResource();
+
+                    protected void setActionBarIcon(int iconRes) {
+                    toolbar.setNavigationIcon(iconRes);
+                }
+
+                    public void setActionBarTitle(int title) {
+                    getSupportActionBar().setTitle(title);
+               }
+
+                           public void setActionBarTitle(String title) {
+                    getSupportActionBar().setTitle(title);
+               }
+
+                   @Override
+            public boolean onCreateOptionsMenu(Menu menu) {
+                    // Inflate the menu; this adds items to the action bar if it is present.
+                            getMenuInflater().inflate(R.menu.menu_main, menu);
+                    return true;
+        }
+        @Override
+          public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+                    // automatically handle clicks on the Home/Up button, so long
+                    // as you specify a parent activity in AndroidManifest.xml.
+                                          int id = item.getItemId();
+
+                            //noinspection SimplifiableIfStatement
+                                    if (id == R.id.action_settings) {
+                            return true;
+                        }
+
+                           return super.onOptionsItemSelected(item);
+                }
+
+            }
+
